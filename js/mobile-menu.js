0f6c74f8ae38600025e3876bd367a19bd90f5810
expiko <masak988@gmail.com>
2022-06-07 17:10:12 +0400

    Скрипт для открывания и закрывания меню на мобильной версии

---
+++ js/mobile-menu.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var navigationMain = document.querySelector(".main-navigation");
+var navigationToggle = document.querySelector(".main-navigation__toggle");
+
+navigationMain.classList.remove("main-navigation--nojs");
+navigationToggle.addEventListener("click", function () {
+	if (navigationMain.classList.contains("main-navigation--closed")) {
+		navigationMain.classList.remove("main-navigation--closed");
+		navigationMain.classList.add("main-navigation--opened");
+	} else {
+		navigationMain.classList.add("main-navigation--closed");
+		navigationMain.classList.remove("main-navigation--opened"); 
+	}
+});
